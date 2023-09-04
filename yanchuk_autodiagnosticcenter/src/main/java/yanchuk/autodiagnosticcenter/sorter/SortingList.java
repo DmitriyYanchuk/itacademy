37408a5137f4becb68e3033104958944f559f593
Dmitriy Yanchuk <nice.skill.2015@gmail.com>
2023-09-04 21:25:49 +0300

    remove old

--- yanchuk_autodiagnosticcenter/src/main/java/yanchuk/autodiagnosticcenter/sorter/SortingList.java
+++
@@ -1,8 +0,0 @@
-package yanchuk.autodiagnosticcenter.sorter;
-
-import java.util.List;
-
-public interface SortingList {
-
-    List<String> sorting(List<String> list) throws SortingListException;
-}
